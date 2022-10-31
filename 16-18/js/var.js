afef5dc8e644ad5cd4ce4c598b7f788c579b31a8
Nursaidbek <qnursaidbek@gmail.com>
2022-10-31 14:46:43 +0800

    i have learned properties and metods. That's will do.

--- 16-18/js/var.js
+++ 16-18/js/var.js
@@ -65,3 +65,40 @@
 //     return "Hello world"
 // }
 // console.log(addition(10, 5));
+
+// Property metods
+// lenght property is for str 
+const user = "Sarvar"
+console.log(user.length);
+// length property is for arr
+const nums = [1, 2, 3, 4, 5]
+console.log(nums.length)
+    // if we want to get letter from index
+console.log(user[2]);
+
+// touppercase is do all letters in upper but variable's value is not upper
+console.log(user.toUpperCase());
+console.log(user);
+
+// if we want upper one we must create new variables
+const upperUser = user.toUpperCase()
+console.log(upperUser);
+
+// tolowercase is do all letters in lower but variable's value is not upper
+console.log(user.toLowerCase());
+
+// if we want lower one we must create new variables
+const greeting = "Hello World";
+// indexOf metod is to get index of letter in parenthesis
+console.log(greeting.indexOf("W"));
+// slice metod is to get word in str variables from index in first letter. Index must be minus or plus
+console.log(greeting.slice(5));
+// substring metod is like slice but it is not worked with minus index
+console.log(greeting.substring(0, 5));
+
+// integer metods and properties
+const num = 8.8
+console.log(Math.round(num));
+const borderWidth = "13.8px";
+// console.log(parseInt(borderWidth));
+console.log(parseFloat(borderWidth));
