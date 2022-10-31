53adf67ec70a662648a89e5378858e6812d11a3c
Nursaidbek <qnursaidbek@gmail.com>
2022-10-31 13:57:48 +0800

    I have learned function theme today 31.10.2022

---
+++ 16-18/js/var.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// function sayHelloWorld() {
+//     console.log("Hello World");
+// };
+
+// sayHelloWorld();
+
+// Function with argument
+// function sayHelloWorld(text) {
+//     console.log(text);
+// };
+
+// sayHelloWorld("Hello World");
+// sayHelloWorld("Hello Javascript");
+
+// function calc(a, b) {
+//     console.log(a + b);
+// };
+// calc(10, 5);
+// calc(10, 9);
+
+// function sayMyName(name, surname) {
+//     console.log(`My name is ${name} ${surname}`);
+// }
+// sayMyName("James", "Arthur")
+
+// Local va global variables is defined
+
+// return
+// function calc(a, b) {
+//     return a + b;
+//     console.log("Check"); // this code is not worked
+// };
+// console.log(calc(10, 5));
+
+// function calc(a, b) {
+//     return a + b;
+// };
+// console.log(calc(10, 5));
+// const sumOfAandB = calc(10, 5);
+// console.log(sumOfAandB);
+
+// Function declaration - we can declare our function before function inventing 
+// console.log(calc(10, 5));
+// const sumOfAandB = calc(10, 5);
+// console.log(sumOfAandB);
+
+// function calc(a, b) {
+//     return a + b;
+// };
+
+// function expression - we can declare our function after function inventing
+// const logger = function() {
+//     console.log("Hello World");
+// }
+// logger()
+
+// Arrow function
+// const calc = (a, b) => a + b;
+// console.log(calc(10, 6));
+// it is also true
+// const addition = (a, b) => {
+//     console.log(a + b);
+//     return "Hello world"
+// }
+// console.log(addition(10, 5));
